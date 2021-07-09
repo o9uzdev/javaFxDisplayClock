f331f485c22fb2263b14c7b6995376fbad56a4a1
o9uzdev <o9uzdev@gmail.com>
2021-07-09 18:52:45 +0300

    Added Application Skeleton

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module JavaFxDisplayClock {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    opens com.o9uzdev.javaFxDisplayClock;
+}
