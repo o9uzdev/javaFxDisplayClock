a20f8df6e292f62f3c442db7017d4d0f336c0705
o9uzdev <o9uzdev@gmail.com>
2021-07-10 22:18:41 +0300

    Added timeStrin, label and create scene, stage

--- src/com/o9uzdev/javaFxDisplayClock/App.java
+++ src/com/o9uzdev/javaFxDisplayClock/App.java
@@ -1,11 +1,21 @@
 package com.o9uzdev.javaFxDisplayClock;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        Pane root = new Pane();
+        String timeString = "00:00:00";
+        Label lblTime = new Label(timeString);
 
+        Scene scene = new Scene(root, 250, 250);
+        stage.setScene(scene);
+        stage.setTitle("Display Clock");
+        stage.show();
     }
 }
