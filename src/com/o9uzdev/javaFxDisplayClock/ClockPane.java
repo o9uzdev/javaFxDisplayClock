4cb3ed7fd6547d89d90c22937d3c84c79c5a4046
o9uzdev <o9uzdev@gmail.com>
2021-07-13 21:02:49 +0300

    Added paintClock method in ClockPane class

--- src/com/o9uzdev/javaFxDisplayClock/ClockPane.java
+++ src/com/o9uzdev/javaFxDisplayClock/ClockPane.java
@@ -1,6 +1,12 @@
 package com.o9uzdev.javaFxDisplayClock;
-
+import javafx.scene.Group;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+
+import java.time.LocalDateTime;
 
 public class ClockPane extends Pane {
     private int hour;
@@ -8,6 +14,7 @@ public class ClockPane extends Pane {
     private int second;
 
     public ClockPane(){
+        setCurrentTime();
     }
 
     public ClockPane(int hour, int minute, int second) {
@@ -20,11 +27,63 @@ public class ClockPane extends Pane {
         return hour;
     }
 
+    public void setHour(int hour) {
+        this.hour = hour;
+        paintClock();
+    }
+
     public int getMinute() {
         return minute;
     }
 
+    public void setMinute(int minute) {
+        this.minute = minute;
+        paintClock();
+    }
+
     public int getSecond() {
         return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+        paintClock();
+    }
+
+    private void setCurrentTime(){
+        LocalDateTime now = LocalDateTime.now();
+        hour = now.getHour();
+        minute = now.getMinute();
+        second = now.getSecond();
+        paintClock();
+    }
+
+    public String getTimeString(){
+        return "";
+    }
+
+    private void paintClock(){
+        double clockRadius = Math.min(getHeight(), getWidth()) * 0.5 * 0.8;
+        double centerX = getWidth()/2;
+        double centerY = getHeight()/2;
+
+        Circle circle = new Circle(centerX, centerY, clockRadius);
+        circle.setFill(Color.WHITE);
+        circle.setStroke(Color.BLACK);
+
+        getChildren().clear();
+        getChildren().addAll(circle);
+    }
+
+    @Override
+    protected void setWidth(double value) {
+        super.setWidth(value);
+        paintClock();
+    }
+
+    @Override
+    protected void setHeight(double value) {
+        super.setHeight(value);
+        paintClock();
     }
 }
